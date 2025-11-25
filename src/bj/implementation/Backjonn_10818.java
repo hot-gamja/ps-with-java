7e3f9281657f3f011e5def08a8196c9f84317a77
hot-gamja <coke0701@gmail.com>
2025-11-25 17:49:05 +0900

    fix: boj_10818.java

--- src/bj/implementation/Backjonn_10818.java
+++ src/bj/implementation/Backjonn_10818.java
@@ -1,7 +1,25 @@
 package bj.implementation;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Backjonn_10818 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list=new ArrayList<>();
+        for(int i=0; i<n; i++){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list); 
+        System.out.print(list.get(0)+" ");
+        System.out.print(list.get(n));
+        
         
     }
     
