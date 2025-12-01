0d91a3a618e05da2d91afca95a7f41620428ebfa
hot-gamja <coke0701@gmail.com>
2025-12-01 17:34:01 +0900

    fix:boj_10809

--- src/bj/implementation/backjoon_10809.java
+++ src/bj/implementation/backjoon_10809.java
@@ -2,23 +2,30 @@ package bj.implementation;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class backjoon_10809 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
         String s = br.readLine();
-        int n = s.length();
 
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            list.add(String.valueOf(s.charAt(i)));
+        int[] pos = new int[26];
+        Arrays.fill(pos, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int idx = ch - 'a'; //0~25
+
+          if(pos[idx] == -1){
+            pos[idx] = i;
+          }
         }
-        list.stream().forEach(System.out::println);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<26; i++){
+            sb.append(pos[i]).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+
 
     }
 }
