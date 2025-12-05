0a6238c6fdc69c3ab03122f4c69d050be18df79d
hot-gamja <coke0701@gmail.com>
2025-12-05 16:32:21 +0900

    faat:add boj_1356

---
+++ src/bj/math/Backjoon_1356.java
@@ -0,0 +1,13 @@
+package bj.math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class Backjoon_1356 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+    
+}
