9b89da9ae9b831cdc8667365e3e62d2d7de7af26
hot-gamja <coke0701@gmail.com>
2026-01-15 02:09:24 +0900

    boj1920

--- src/bj/binary_search/Backjoon_1920.java
+++ src/bj/binary_search/Backjoon_1920.java
@@ -6,21 +6,52 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Backjoon_1920 {
+    static boolean binarySearch(int[] arr, int target) {
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return false;
+
+    }
+
     public static void main(String[] ars) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
         int[] arrN = new int[N];
-        int[] arrM = new int[M];
-
         for (int i = 0; i < N; i++) {
-            arrN[i] = Integer.parseInt(br.readLine());
-        }
-        for(int i=0; i<M; i++){
-            arrM[M] = Integer.parseInt(br.readLine());
+            arrN[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arrN);
+        
+        int M = Integer.parseInt(br.readLine());
+        int[] arrM = new int[M];
+        for (int i = 0; i < M; i++) {
+            arrM[M] = Integer.parseInt(st.nextToken()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<M; i++){
+            int x = arrM[i];
+            if(binarySearch(arrN, x)) {
+                sb.append(1).append('\n');
+            }else{
+                sb.append(0).append('\n');
+            }
+            System.out.print(sb.toString());
+        }
+
     }
 }
