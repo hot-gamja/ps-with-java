a68f71b3afb717ffef191e7b18be1bd01fbec564
hot-gamja <coke0701@gmail.com>
2026-01-19 17:42:44 +0900

    boj_2789 두번째 풀이중

---
+++ src/bj/bruteforcing/Backjoon_2789Ver2.java
@@ -0,0 +1,23 @@
+package bj.bruteforcing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Backjoon_2789Ver2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st; 
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] cards = new int [N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++) {
+            cards [i] = Integer.parseInt(st.nextToken());
+        }
+        int best = 0;
+        for(int i=0; i<)
+    }
+}
