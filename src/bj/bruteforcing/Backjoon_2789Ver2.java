36afd7b6dca12b4ed78defe30fcdc63d81ed17a5
hot-gamja <coke0701@gmail.com>
2026-01-20 00:17:57 +0900

    boj_2789ver2

--- src/bj/bruteforcing/Backjoon_2789Ver2.java
+++ src/bj/bruteforcing/Backjoon_2789Ver2.java
@@ -7,17 +7,28 @@ import java.util.StringTokenizer;
 public class Backjoon_2789Ver2 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st; 
+        StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] cards = new int [N];
+        int[] cards = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<N; i++) {
-            cards [i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
         }
         int best = 0;
-        for(int i=0; i<)
+        for (int i = 0; i <N ; i++) {
+            for(int j = i+1; j <N-1; j++){
+                for(int k = j+1; k < N-2 ; k++){
+                    int sum = cards[i] + cards[j] + cards[k];
+                    
+                    if(sum<=M&&best<sum){
+                        best=sum;
+                    }
+                }
+            }
+        }
+        System.out.println(best);
     }
 }
