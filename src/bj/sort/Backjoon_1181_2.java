8c7c02da354d98a779217741a1c1618a4e06321d
hot-gamja <coke0701@gmail.com>
2026-01-22 17:41:06 +0900

    boj_1181

---
+++ src/bj/sort/Backjoon_1181_2.java
@@ -0,0 +1,22 @@
+package bj.sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class Backjoon_1181_2 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+        List<String> list = new ArrayList<>(set);
+
+    }
+}
