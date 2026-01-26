b0791cb49c52daa28da56121d6dc6288a44df0c4
hot-gamja <coke0701@gmail.com>
2026-01-26 17:44:18 +0900

    boj_7568

---
+++ src/bj/implementation/Backjoon_7568.java
@@ -0,0 +1,21 @@
+package bj.implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Backjoon_7568 {
+    static class body {
+        int x;
+        int y;
+
+        body(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
