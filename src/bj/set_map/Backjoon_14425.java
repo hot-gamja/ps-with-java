9a442b4b5269a4e69b52fb35d8b294928c7df5df
hot-gamja <coke0701@gmail.com>
2026-01-28 17:38:27 +0900

    bj14425

---
+++ src/bj/set_map/Backjoon_14425.java
@@ -0,0 +1,29 @@
+package bj.set_map;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Backjoon_14425 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            String s = br.readLine();
+            if (set.contains(s))
+                count++;
+        }
+        System.out.print(count);
+
+    }
+}
