f27358427c6859e116f177b98b63168165678c80
hot-gamja <coke0701@gmail.com>
2026-03-06 02:11:04 +0900

    boj_17219

---
+++ src/bj/set_map/Backjoon_17219.java
@@ -0,0 +1,36 @@
+package bj.set_map;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Backjoon_17219 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String site = st.nextToken();
+            String pw = st.nextToken();
+
+            map.put(site, pw);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            String query = br.readLine();
+
+            sb.append(map.get(query)).append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}
